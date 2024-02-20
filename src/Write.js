d462c2ded0f54bb29715f1bc1894791712b5b3ff
JangSE123 <148615025+JangSE123@users.noreply.github.com>
2024-02-20 16:05:15 +0900

    image
    image file upload

--- src/Write.js
+++ src/Write.js
@@ -8,16 +8,31 @@ export default function Write(props) {
     title: "",
     content: "",
     price: "",
-    image: "",
+    image: "", // 이미지 파일의 경로를 저장할 변수 추가
     item: ""
   });
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
-    setNewPost({
-      ...newPost,
-      [name]: value,
-    });
+    if (name === 'image') {
+      // 파일 선택 시 파일 내용을 읽고 업로드
+      const file = e.target.files[0];
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        // 파일의 내용을 읽어와서 이미지 경로를 설정
+        setNewPost({
+          ...newPost,
+          image: reader.result, // 파일 내용을 이미지 경로로 설정
+        });
+      };
+      reader.readAsDataURL(file);
+    } else {
+      // 이미지가 아닌 다른 입력 필드는 기존과 동일하게 처리
+      setNewPost({
+        ...newPost,
+        [name]: value,
+      });
+    }
   };
 
   const handleSubmit = () => {
@@ -33,12 +48,10 @@ export default function Write(props) {
       like: false 
     };
 
-
     props.setTopics(prevTopics => [...prevTopics, newTopic]);
     console.log("Updated topics in Main:", props.topics);
     alert("작성 완료 !");
     
-
     setNewPost({  
       name: "",
       profile: '',
@@ -87,7 +100,6 @@ export default function Write(props) {
         <button className='okay' onClick={handleSubmit}>작성</button>
         <button className='okay'>취소</button>
       </div>
-      
     </div>
   );
 }
